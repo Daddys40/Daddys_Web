9dba3af81a5285582bf310447c7906e3a4489e68
Kurt Sang Hyun Lee <breath103@gmail.com>
2014-10-18 15:31:22 +0900

    fix fitler

--- app/assets/javascripts/angular/filters/filter.js
+++ app/assets/javascripts/angular/filters/filter.js
@@ -1,11 +1,13 @@
 angular.module('daddysFilters', [])
   .filter('notificationsDays', function () {
  		return function (input) {
- 			days = ["일", "월", "화", "수", "목", "금", "토"];
- 			days.forEach(function(value, key) {
-				input = input.replace(key.toString(), value); 				
- 			});
- 			console.log(input);
+ 			if (input && input.length > 0) {
+	 			days = ["일", "월", "화", "수", "목", "금", "토"];
+	 			days.forEach(function(value, key) {
+					input = input.replace(key.toString(), value); 				
+	 			});
+	 			console.log(input);
+ 			}
  			return input;
  		}
   });  
